69f3949e1cae910f9452a329f2064a5e006b5a20
Tom Gilligan <thomasgilligan@icloud.com>
2023-07-24 20:31:10 +1000

    Move out markdown options

--- src/main.rs
+++ src/main.rs
@@ -14,61 +14,8 @@ pub struct Frontmatter {
     pub title: String
 }
 
-const MARKDOWN_OPTIONS: markdown::Options = markdown::Options {
-    compile: markdown::CompileOptions {
-        allow_dangerous_html: true,
-        allow_dangerous_protocol: false,
-        default_line_ending: markdown::LineEnding::LineFeed,
-        gfm_footnote_back_label: None,
-        gfm_footnote_clobber_prefix: None,
-        gfm_footnote_label: None,
-        gfm_footnote_label_attributes: None,
-        gfm_footnote_label_tag_name: None,
-        gfm_tagfilter: false,
-    },
-    parse: markdown::ParseOptions {
-        constructs: markdown::Constructs {
-            attention: true,
-            gfm_autolink_literal: true,
-            autolink: false,
-            block_quote: true,
-            character_escape: true,
-            character_reference: false,
-            code_indented: true,
-            code_fenced: true,
-            code_text: true,
-            definition: true,
-            frontmatter: true,
-            gfm_footnote_definition: true,
-            gfm_label_start_footnote: true,
-            gfm_strikethrough: true,
-            gfm_table: true,
-            gfm_task_list_item: false,
-            hard_break_escape: false,
-            hard_break_trailing: false,
-            html_text: false,
-            html_flow: false,
-            heading_setext: false,
-            heading_atx: true,
-            label_start_image: true,
-            label_start_link: true,
-            label_end: true,
-            list_item: true,
-            math_flow: true,
-            math_text: true,
-            mdx_esm: false,
-            mdx_expression_flow: false,
-            mdx_expression_text: false,
-            mdx_jsx_flow: false,
-            mdx_jsx_text: false,
-            thematic_break: true,
-        },
-        gfm_strikethrough_single_tilde: true,
-        math_text_single_dollar: true,
-        mdx_expression_parse: None,
-        mdx_esm_parse: None,
-    },
-};
+mod markdown_options;
+use crate::markdown_options::MARKDOWN_OPTIONS;
 
 markup::define! {
     Layout<'a>(content: &'a str, frontmatter: Frontmatter) {
