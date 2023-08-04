ec8356ea6be03cc285be5d3272432171f839b6e8
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-08-04 22:06:49 +1000

    Remove CSPs rejected by Chrome

--- src/layout.rs
+++ src/layout.rs
@@ -21,7 +21,7 @@ markup::define! {
             head {
                 title { @combined_title(title, *page_title) }
                 meta[charset = "utf-8"];
-                meta["http-equiv" = "Content-Security-Policy", content = "default-src 'none'; script-src 'none'; script-src-elem 'self'; script-src-attr 'none'; style-src 'none'; style-src-elem 'unsafe-inline'; style-src-attr 'none'; img-src 'self' data:; font-src 'none'; connect-src 'none'; media-src 'none'; object-src 'none'; prefetch-src *; child-src 'none'; frame-src 'none'; worker-src 'none'; frame-ancestors 'none'; form-action 'none'; upgrade-insecure-requests; block-all-mixed-content; sandbox allow-same-origin allow-scripts allow-top-navigation; base-uri 'self'; manifest-src 'self'"];
+                meta["http-equiv" = "Content-Security-Policy", content = "default-src 'none'; script-src 'none'; script-src-elem 'self'; script-src-attr 'none'; style-src 'none'; style-src-elem 'unsafe-inline'; style-src-attr 'none'; img-src 'self' data:; font-src 'none'; connect-src 'none'; media-src 'none'; object-src 'none'; child-src 'none'; frame-src 'none'; worker-src 'none'; form-action 'none'; upgrade-insecure-requests; block-all-mixed-content; base-uri 'self'; manifest-src 'self'"];
                 meta[name = "author", content = author];
                 meta[name = "description", content = description];
                 link[rel = "alternate", r#type = "application/rss+xml", href = "pages.xml", title = title];
