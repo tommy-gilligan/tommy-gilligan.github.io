2c76855def147a2ffcdf6a8a9ecbce99839eb3f3
Tom Gilligan <thomasgilligan@icloud.com>
2023-08-07 14:33:10 +1000

    Revert "Refuse to start if chromedriver.log exists"
    This reverts commit b82436439a2cbe41cc7b7796d6a436e02c84bdf0.
    
    On CI chromedriver is currently started too early for this to make sense

--- src/chrome_driver.rs
+++ src/chrome_driver.rs
@@ -16,7 +16,6 @@ pub struct ChromeDriver {
 
 impl ChromeDriver {
     pub async fn new() -> Self {
-        assert!(!Path::new("chromedriver.log").exists());
         let mut caps = DesiredCapabilities::chrome();
         // GPU doesn't get used on GH runner
         caps.set_disable_gpu().unwrap();
