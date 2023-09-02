b02144affc957125252474928382b85a4fb533f0
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-09-02 10:52:24 +1000

    Lint

--- toolkit/src/shell.rs
+++ toolkit/src/shell.rs
@@ -2,7 +2,6 @@ use std::process::{Child, ExitStatus, Stdio};
 
 use tokio::process::Command;
 
-#[must_use]
 pub async fn spawn(command: &str) -> std::io::Result<ExitStatus> {
     if cfg!(target_os = "windows") {
         Command::new("cmd")
